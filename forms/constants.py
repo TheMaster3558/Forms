e5ceccdc7310c6b8d14d475a92916c218376f840
chawkk6404 <chawk_k@outlook.com>
2022-11-06 11:43:52 -0800

    Support sphinx

--- forms/constants.py
+++ forms/constants.py
@@ -1,6 +1,12 @@
-from typing import Final
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 import discord
+
+# sphinx doesn't support typing.Final due to python version
+if TYPE_CHECKING:
+    from typing import Final
 
 COLOR: Final[int] = 0x2F3136
 CONFIG_PATH: Final[str] = './config.json'
